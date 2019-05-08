1079444d79fabe327a05116f39eac23e0e4ccdee
AnriTapel <anri.snep@gmail.com>
2019-05-08 08:40:27 +0300

    Initial commit

---
+++ src/main/java/hello/WebConfig.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by ANRI on 07.05.2019.
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/styles/**", "/static/**").addResourceLocations("classpath:/styles/", "classpath:/static/");
+    }
+
+
+}
